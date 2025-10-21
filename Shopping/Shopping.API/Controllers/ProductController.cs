57b492a5776d4e0fbdea446858d59704902da12d
Ergashev Javlon <mengliboyevzohidbek@gmail.com>
2025-10-22 01:15:26 +0500

    many changes

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 using Shopping.API.Data;
 using Shopping.API.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace Shopping.API.Controllers
 {
@@ -10,17 +12,21 @@ namespace Shopping.API.Controllers
     [Route("[controller]")]
     public class ProductController
     {
+        private readonly ProductContext _context;
         private readonly ILogger<ProductController> _logger;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
         {
             _logger = logger;   
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<Product> Get()
+        public async Task<IEnumerable<Product>> GetAsync()
         {
-            return ProductContext.Products;
+            return await _context.Products
+                                 .Find(p => true)
+                                 .ToListAsync();
         }
     }
 }
--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -1,16 +1,37 @@
 ﻿using Shopping.API.Models;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace Shopping.API.Data
 {
-    /// <summary>
-    /// Hard coded static data
-    /// </summary>
-    public static class ProductContext
+    public class ProductContext
     {
-        public static readonly List<Product> Products = new List<Product>
+        public ProductContext(IConfiguration configuration)
         {
-            new Product()
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+
+        private static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
                 {
                     Name = "IPhone X",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
@@ -18,46 +39,47 @@ namespace Shopping.API.Data
                     Price = 950.00M,
                     Category = "Smart Phone"
                 },
-            new Product()
-            {
-                Name = "Samsung 10",
-                Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                ImageFile = "product-2.png",
-                Price = 840.00M,
-                Category = "Smart Phone"
-            },
-            new Product()
-            {
-                Name = "Huawei Plus",
-                Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                ImageFile = "product-3.png",
-                Price = 650.00M,
-                Category = "White Appliances"
-            },
-            new Product()
-            {
-                Name = "Xiaomi Mi 9",
-                Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                ImageFile = "product-4.png",
-                Price = 470.00M,
-                Category = "White Appliances"
-            },
-            new Product()
-            {
-                Name = "HTC U11+ Plus",
-                Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                ImageFile = "product-5.png",
-                Price = 380.00M,
-                Category = "Smart Phone"
-            },
-            new Product()
-            {
-                Name = "LG G7 ThinQ EndofCourse",
-                Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                ImageFile = "product-6.png",
-                Price = 240.00M,
-                Category = "Home Kitchen"
-            }
-        };
+                new Product()
+                {
+                    Name = "Samsung 10",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Huawei Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-3.png",
+                    Price = 650.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "Xiaomi Mi 9",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-4.png",
+                    Price = 470.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "HTC U11+ Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-5.png",
+                    Price = 380.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "LG G7 ThinQ EndofCourse",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Kitchen"
+                }
+            };
+        }
     }
 }
